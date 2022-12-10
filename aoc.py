81e68d1a738c5e426e0a908c54c83d85c362c468
Andreas <code@andreats.com>
2022-12-10 17:37:57 +0100

    ++ in lib
    add hasduplicates to lib imports

--- aoc.py
+++ aoc.py
@@ -5,3 +5,12 @@ def printGrid(grid):
         print(end="\n")
     print(end="\n")
 
+def hasDuplicates(x):
+    k = 0
+    for l in range(len(x)-1):
+        k=l+1
+        for l in range(k):
+            if x[l]==x[k]:
+                return True
+    return False
+
